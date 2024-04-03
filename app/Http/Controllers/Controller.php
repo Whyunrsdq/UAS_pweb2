2f57f71e203d7743850e83631d3c3b19619ffc08
Whyunrsdq <wahyunursidiq4321@gmail.com>
2024-04-03 14:58:26 +0700

    update

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
+
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
@@ -9,4 +11,16 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+}
+class PegawaiController extends Controller
+{
+    public function index()
+    {
+    	// mengambil data dari table pegawai
+    	$pegawai = DB::table('tabelbarang')->get();
+ 
+    	// mengirim data pegawai ke view index
+    	return view('CRUD',['tabelbarang' => $tabelbarang]);
+ 
+    }
 }
